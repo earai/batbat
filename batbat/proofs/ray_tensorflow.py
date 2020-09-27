0d7319812f1876efad14b145670849dcf5591528
erin <erin.arai@gmail.com>
2020-09-27 12:48:19 -0400

    Committing Pipfiles for environment that has ray, tensorflow, tensorflow-probability, and rrlib

--- batbat/proofs/ray_tensorflow.py
+++ batbat/proofs/ray_tensorflow.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 import ray
-
+from tensorflow_probability import distributions as tfd
 
 def main():
     ray.init()
 
     @ray.remote
     def f(x):
-        return (tf.constant(x) * tf.constant(x)).numpy()
+        return (tfd.Normal(loc=x, scale=x).sample() * tf.constant(x,dtype=tf.float32)).numpy()
 
     futures = [f.remote(i) for i in range(4)]
     print(ray.get(futures)) # [0, 1, 4, 9]
